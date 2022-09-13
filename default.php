7a26d6e24354bb3fef725aecfb56e533d4ef51c5
Luiz Felipe <luizfelipecastrovb@gmail.com>
2022-09-12 21:51:18 -0300

    Added pages

---
+++ default.php
@@ -0,0 +1,166 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>olooeez's portfolio</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="icon" href="images/favicon.png" type="image/x-icon" />
+	</head>
+	<body class="is-preload">
+			<nav id="nav">
+				<ul class="container">
+					<li><a href="#top">Top</a></li>
+					<li><a href="#skills">Skills</a></li>
+					<li><a href="#portfolio">Portfolio</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</nav>
+			<article id="top" class="wrapper style1">
+				<div class="container">
+					<div class="row">
+						<div class="col-4 col-5-large col-12-medium">
+							<span class="image fit"><img src="images/profile-picture.jpg" alt="" /></span>
+						</div>
+						<div class="col-8 col-7-large col-12-medium">
+							<header>
+								<h1>Hi. I'm <strong>Luiz Felipe</strong>.</h1>
+							</header>
+							<p>I'm currently studying information systems at the <a href="https://www.ufv.br/" target="_blank">Federal University of Viçosa</a>. A lover of open source technology and free software, I enjoy developing and contributing to tools that I find useful in this growing industry.</p>
+							<a href="#skills" class="button large scrolly">Learn about what I can do</a>
+						</div>
+					</div>
+				</div>
+			</article>
+			<article id="skills" class="wrapper style2">
+				<div class="container">
+					<header>
+						<h2>Here's all the stuff I can do.</h2>
+						<p>Things I learned inside and outside the academic space.</p>
+					</header>
+					<div class="row aln-center"> 
+						<div class="col-4 col-6-medium col-12-small">
+							<section class="box style1">
+								<span class="icon featured solid fa-code"></span>
+								<h3>Software development</h3>
+								<p>I have knowledge of clean code principles, <a href="https://en.wikipedia.org/wiki/Algorithm", target="_blank">algorithms</a> and <a href="https://en.wikipedia.org/wiki/Data_structure", target="_blank">data structures</a>, <a href="https://en.wikipedia.org/wiki/Object-oriented_programming" target="_blank">object-oriented programming</a>. I know C/C++, Go, Java, JavaScript, Shell Script, Python.</p>
+							</section>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<section class="box style1">
+								<span class="icon featured solid fa-palette"></span>
+								<h3>Design</h3>
+								<p>I know how to make basic edits on images to remove the background, beautify, create banners and posts for social networks, etc. I mainly use <a href="https://www.canva.com/en" target="_blank">Canva</a> and <a href="https://www.gimp.org" target="_blank">GIMP</a></p>
+							</section>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<section class="box style1">
+								<span class="icon featured solid fa-code-branch"></span>
+								<h3>Code management</h3>
+								<p>I use code versioning tools like <a href="https://git-scm.com" target="_blank">Git</a>, code storage platforms like <a href="https://github.com/about", target="_blank">GitHub</a>, <a href="https://about.gitlab.com", target="_blank">GitLab</a> and <a href="https://gitea.io/en-us", target="_blank">Gitea</a>, branching strategies like GitHub and GitLab flow and <a href="https://en.wikipedia.org/wiki/CI/CD" target="_blank">CI/CD</a> techniques.</p>
+							</section>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<section class="box style1">
+								<span class="icon featured solid fa-terminal"></span>
+								<h3>Operational systems</h3>
+								<p>I'm a <a href="https://pt.wikipedia.org/wiki/Sistema_operacional_tipo_Unix" target="_blank">Unix-like</a> operating system enthusiast. I've made contributions to <a href="https://ubuntu.com" target="_blank">Ubuntu</a> and I currently use <a href="https://getfedora.org" target="_blank">Fedora</a> (which I want to make contributions to as well).</p>
+							</section>
+						</div>
+					</div>
+					<footer>
+						<p>Want to see what i'm up to?</p>
+						<a href="#portfolio" class="button large scrolly">See some of my recent work</a>
+					</footer>
+				</div>
+			</article>
+			<article id="portfolio" class="wrapper style3">
+				<div class="container">
+					<header>
+						<h2>Here’s some stuff I made recently.</h2>
+						<p>Proin odio consequat  sapien vestibulum consequat lorem dolore feugiat.</p>
+					</header>
+					<div class="row">
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2">
+								<h3><a href="https://github.com/lureevar/toddo">toddo</a></h3>
+								<p>A simple CLI todo application inspired by todo.txt made in Golang using the cli module.</p>
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2">
+								<h3><a href="https://github.com/lureevar/beecrowd-solutions">beecrowd solutions</a></h3>
+								<p>A repository with the solution of various exercises from various categories from the website breecrowd.</p>
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2">
+								<h3><a href="https://github.com/lureevar/perlweeklychallenge-club">perl weekly challenge</a></h3>
+								<p>A fork to push my perl exercise resolutions from perlweeklychallenge-club.</p>
+							</article>
+						</div>
+					</div>
+					<footer>
+						<p>Want to say something directly to me?</p>
+						<a href="#contact" class="button large scrolly">Get in touch with me</a>
+					</footer>
+				</div>
+			</article>
+			<article id="contact" class="wrapper style4">
+				<div class="container medium">
+					<header>
+						<h2>Have me make stuff for you.</h2>
+						<p>Fill in the form below to send me an email.</p>
+					</header>
+					<div class="row">
+						<div class="col-12">
+							<form action="https://formsubmit.co/contact@olooeez.com.br" method="post" action="#">
+								<div class="row">
+									<input type="hidden" name="_captcha" value="false"/>
+									<input type="hidden" name="_next" value="https://olooeez.com.br/thanks"/>
+									<div class="col-6 col-12-small">
+										<input type="text" name="name" id="name" placeholder="Name" required/>
+									</div>
+									<div class="col-6 col-12-small">
+										<input type="text" name="email" id="email" placeholder="Email" required/>
+									</div>
+									<div class="col-12">
+										<input type="text" name="_subject" id="subject" placeholder="Subject" />
+									</div>
+									<div class="col-12">
+										<textarea name="message" id="message" placeholder="Message"></textarea>
+									</div>
+									<div class="col-12">
+										<ul class="actions">
+											<li><input type="submit" value="Send Message" /></li>
+											<li><input type="reset" value="Clear Form" class="alt" /></li>
+										</ul>
+									</div>
+								</div>
+							</form>
+						</div>
+						<div class="col-12">
+							<hr />
+							<h3>Find me on...</h3>
+							<ul class="social">
+								<li><a href="https://linkedin.com/in/olooeez" target="_blank" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+								<li><a href="https://github.com/olooeez" target="_blank" class="icon brands fa-github"><span class="label">Github</span></a></li>
+							</ul>
+							<hr />
+						</div>
+					</div>
+					<footer>
+						<ul id="copyright">
+							<li>&copy; Luiz Felipe. All rights reserved.</li>
+						</ul>
+					</footer>
+				</div>
+			</article>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+	</body>
+</html>
---
+++ thanks.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Thank You!</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="../assets/css/main.css" />
+	<link rel="icon" href="../images/thanks-favicon.png" type="image/x-icon" />
+</head>
+<body class="is-preload">
+	<article class="wrapper style1" style="box-shadow: none !important">
+		<div class="container">
+			<h1>Thanks for submiting an email</h1>
+			<p>The form was submitted successfully. Return to <a href="https://olooeez.com.br/">original site</a></p>
+		</div>
+	</article>
+</body>
+</html>
